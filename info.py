8d4ad01f81e8a8d5806a086c9a67aa1d77ab4eeb
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-11-20 11:41:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,1 +1,18 @@
+from program  import client,filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+TeluguZone = Client(
+    name = "Daemon",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+
+ print("Bot was started")
+
+ TeluguZone.run()
